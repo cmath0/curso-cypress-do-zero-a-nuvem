1f9629ac7b06d7177cc4c161fd5f113b0098036d
Matheus Cardoso da Silva <matheus97_mcs@hotmail.com>
2025-11-06 18:59:25 -0300

    Aulas 01 a 04 - Localizar, digitar, clicar e manipular elementos

---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,137 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+    cy.visit('./src/index.html')
+  })
+
+  it('verifica o título da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = Cypress._.repeat('abcdefgh ', 10)
+
+    cy.get('input[id="firstName"]').type('Matheus', {delay: 0})
+    cy.get('input[id="lastName"]').type('Cardoso', {delay: 0})
+    cy.get('input[id="email"]').type('emaildomatheus@teste.com', {delay: 0})
+    cy.get('textarea[id="open-text-area"]').type(longText, {delay: 0})
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('span[class="success"').should('be.visible')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('input[id="firstName"]').type('Matheus', {delay: 0})
+    cy.get('input[id="lastName"]').type('Cardoso', {delay: 0})
+    cy.get('input[id="email"]').type('emaildomatheusteste', {delay: 0})
+    cy.get('textarea[id="open-text-area"]').type('Teste de texto de feedback.', {delay: 0})
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('span[class="error"').should('be.visible')
+  })
+
+  it('não permite digitar valores não numéricos no campo de telefone', () => {
+    cy.get('input[id="phone"]').type('telInvalido', {delay: 0})
+    cy.get('input[id="phone"]').type('-', {delay: 0})
+    cy.get('input[id="phone"]').type('(', {delay: 0})
+    cy.get('input[id="phone"]').type(')', {delay: 0})
+
+    cy.get('input[id="phone"]').should('have.value', '')
+  })
+
+  
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    // Preenchendo campos obrigatórios
+    cy.get('input[id="firstName"]').type('Matheus', {delay: 0})
+    cy.get('input[id="lastName"]').type('Cardoso', {delay: 0})
+    cy.get('input[id="email"]').type('emaildomatheus@teste.com', {delay: 0})
+    cy.get('textarea[id="open-text-area"]').type('Teste de texto de feedback.', {delay: 0})
+    
+    // Habilitando checkbox telefone, logo o campo de telefone deve se tornar obrigatório
+    cy.get('input[id="phone-checkbox"]').click()
+
+    // Submeter formulário sem preencher telefone
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('span[class="error"').should('be.visible')
+  })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    // preencher campos
+    cy.get('input[id="firstName"]').type('Matheus', {delay: 0})
+    cy.get('input[id="lastName"]').type('Cardoso', {delay: 0})
+    cy.get('input[id="email"]').type('emaildomatheus@teste.com', {delay: 0})
+    cy.get('input[id="phone"]').type('999556644', {delay: 0})
+    
+    // validar campos preenchidos
+    cy.get('input[id="firstName"]').should('have.value', 'Matheus')
+    cy.get('input[id="lastName"]').should('have.value', 'Cardoso')
+    cy.get('input[id="email"]').should('have.value', 'emaildomatheus@teste.com')
+    cy.get('input[id="phone"]').should('have.value', '999556644')
+
+    // limpar campos
+    cy.get('input[id="firstName"]').clear()
+    cy.get('input[id="lastName"]').clear()
+    cy.get('input[id="email"]').clear()
+    cy.get('input[id="phone"]').clear()
+
+    // validar campos vazios
+    cy.get('input[id="firstName"]').should('have.value', '')
+    cy.get('input[id="lastName"]').should('have.value', '')
+    cy.get('input[id="email"]').should('have.value', '')
+    cy.get('input[id="phone"]').should('have.value', '')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('span[class="error"').should('be.visible')
+  })
+
+  it('envia o formulário com sucesso usando um comando customizado', () => {
+    const data = {
+      firstName: 'Lucia',
+      lastName: 'Rodrigues',
+      email: 'lucia.rodrigues@emaildetestes.com',
+      feedback: 'Feedback da Lucia.'
+    }
+
+    cy.fillMandatoryFieldsAndSubmitWithDefaultObject(data)
+    
+    cy.get('span[class="success"').should('be.visible')
+  })
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+  })
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+  })
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+  })
+
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[name="atendimento-tat"]').check('feedback')
+
+    cy.get('input[name="atendimento-tat"][value="feedback"]')
+      .should('be.checked')
+      .and('have.value', 'feedback')
+  })
+
+  it('marca cada tipo de atendimento', () => {
+    cy.get('input[name="atendimento-tat"]')
+      .each((tipoAtendimento) => {
+        cy.wrap(tipoAtendimento)
+          .check()
+          .should('be.checked')
+      })
+  })
+})
